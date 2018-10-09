a28d03889ba730c36813bd13bd34896f1ae2e9ee
kristopherbullinger <github email address>
2018-10-09 08:49:37 -0400

    Lab Complete

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  post '/' do
+    @puppy = params
+    erb :display_puppy
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
 
 end
